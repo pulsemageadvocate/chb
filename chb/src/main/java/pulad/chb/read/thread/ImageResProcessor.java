6cd2a0aa4e6a281b2a189a01637363b2a2631d54
pulad <pulad@D>
2020-04-07 14:01:14 +0900

    Twitterの画像URL(pbs.twimg.com)に対応。

--- chb/src/main/java/pulad/chb/read/thread/ImageResProcessor.java
+++ chb/src/main/java/pulad/chb/read/thread/ImageResProcessor.java
@@ -10,14 +10,16 @@ import pulad.chb.dto.ResDto;
 import pulad.chb.interfaces.ResProcessor;
 
 /**
- * 画像を抽出する
+ * 画像URLを抽出する
  * @author pulad
  *
  */
 public class ImageResProcessor implements ResProcessor {
 
 	// immutable
-	private final Pattern regBody = Pattern.compile("h?ttp(s?://[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-]+((?<=\\.jp(e|g|eg))|(?<=\\.png)|(?<=\\.gif)))", Pattern.CASE_INSENSITIVE);
+	private final Pattern regBody = Pattern.compile("h?ttp(s?://[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-\\;]+((?<=\\.jp(e|g|eg))|(?<=\\.png)|(?<=\\.gif)))", Pattern.CASE_INSENSITIVE);
+	private final Pattern regBodyTwitter = Pattern.compile("h?ttp(s?://pbs\\.twimg\\.com/media/(?=.*format=(jp(e|g|eg))|png|gif)[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-\\;]+)", Pattern.CASE_INSENSITIVE);
+	private final Pattern regUnsanitize = Pattern.compile("\\&amp;");
 
 	@Override
 	public void process(String url, TreeMap<Integer, ResDto> res, boolean remote, long now) {
@@ -34,8 +36,21 @@ public class ImageResProcessor implements ResProcessor {
 			while (matcher.find()) {
 				// remoteでなくてもレスポップアップの場合に読み込みしたいので
 				//images.add((remote ? LocalURLStreamHandler.PROTOCOL : LocalURLStreamHandler.PROTOCOL_LOCAL) + matcher.group(1));
-				images.add(LocalURLStreamHandler.PROTOCOL + matcher.group(1));
+				images.add(unsanitize(LocalURLStreamHandler.PROTOCOL + matcher.group(1)));
+			}
+			matcher = regBodyTwitter.matcher(dto.getBody());
+			while (matcher.find()) {
+				images.add(unsanitize(LocalURLStreamHandler.PROTOCOL + matcher.group(1)));
 			}
 		}
+	}
+
+	/**
+	 * 無害化済みのURLを元に戻す。
+	 * @param str
+	 * @return
+	 */
+	private String unsanitize(String str) {
+		return regUnsanitize.matcher(str).replaceAll("&");
 	}
 }
