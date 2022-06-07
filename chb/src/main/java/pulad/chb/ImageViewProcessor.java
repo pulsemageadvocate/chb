5ae15d135277aa403b893e98f56c354565ac1e2a
pulsemageadvocate <regist@plasticpop.mydns.jp>
2022-06-07 14:22:33 +0900

    画像ポップアップに拡大倍率を表示。

--- chb/src/main/java/pulad/chb/ImageViewProcessor.java
+++ chb/src/main/java/pulad/chb/ImageViewProcessor.java
@@ -4,12 +4,14 @@ import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Popup;
 import javafx.stage.Screen;
 import javafx.stage.Window;
@@ -51,10 +53,13 @@ public class ImageViewProcessor {
 		double screenWidth = maxX - minX;
 		double screenHeight = maxY - minY;
 		double scale = 1d;
+		int scaleNumerator = 1;
+		int scaleDenominator = 1;
 		if (screenWidth >= 1d || screenHeight >= 1d) {
 			while (imageWidth * scale > screenWidth ||
 					imageHeight * scale > screenHeight) {
 				scale /= 2d;
+				scaleDenominator <<= 1;
 			}
 		}
 
@@ -63,40 +68,50 @@ public class ImageViewProcessor {
 		scene.getStylesheets().add(Config.styleCss);
 
 		ImageView imageView = new ImageView(image);
-		// 縮小時は中央に縮小、拡大時は右下へ伸びるのを右下へ統一する。
-		imageView.setFitWidth(image.getWidth() / 16d);
-		imageView.setFitHeight(image.getHeight() / 16d);
-		imageView.setScaleX(imageView.getScaleX() * scale * 16d);
-		imageView.setScaleY(imageView.getScaleY() * scale * 16d);
-//		AnchorPane rootPane = new AnchorPane(imageView);
-//		rootPane.setStyle("-fx-background-color: black;");
-//		rootPane.setPadding(new Insets(1d));
-//		AnchorPane.setLeftAnchor(imageView, 100d);
-//		AnchorPane.setTopAnchor(imageView, 100d);
-//		AnchorPane.setRightAnchor(imageView, 100d);
-//		AnchorPane.setBottomAnchor(imageView, 100d);
-//		scene.setRoot(rootPane);
+		imageView.setFitWidth(image.getWidth());
+		imageView.setFitHeight(image.getHeight());
 
-//		Pane rootPane = new Pane(imageView);
-		AnchorPane rootPane = new AnchorPane(imageView);
-		rootPane.setStyle("-fx-background-color: black;");
+		AnchorPane anchorPane = new AnchorPane(imageView);
+		anchorPane.setStyle("-fx-background-color: black;");
 		AnchorPane.setLeftAnchor(imageView, 0d);
 		AnchorPane.setTopAnchor(imageView, 0d);
 		AnchorPane.setRightAnchor(imageView, 0d);
 		AnchorPane.setBottomAnchor(imageView, 0d);
+
+		ScaleTextPane scaleTextPane = new ScaleTextPane();
+		scaleTextPane.setStyle("-fx-background-color: white;");
+		scaleTextPane.setScaleText(scaleNumerator, scaleDenominator);
+		scaleTextPane.setOpacity(0.75d);
+
+		Pane rootPane = new Pane(anchorPane, scaleTextPane);
 		scene.setRoot(rootPane);
 
 		scene.setOnMouseClicked(new MouseClickEventHandler(popup));
 		MouseDragOverEventHandler dragOverEventHandler = new MouseDragOverEventHandler(popup);
-		imageView.setOnMouseDragOver(dragOverEventHandler);
-		imageView.setOnDragDetected(new DragDetectedEventHandler(popup, imageView, dragOverEventHandler));
-		imageView.setOnScroll(new MouseScrollEventHandler(imageView));
+		rootPane.setOnMouseDragOver(dragOverEventHandler);
+		rootPane.setOnDragDetected(new DragDetectedEventHandler(popup, imageView, dragOverEventHandler));
+		rootPane.setOnScroll(new MouseScrollEventHandler(imageView, scaleTextPane, scaleNumerator, scaleDenominator));
 		// 端で止まらないようにする
 		popup.setAutoFix(false);
 		popup.setAutoHide(true);
 
 		popup.show(owner);
 		return popup;
+	}
+
+	private static class ScaleTextPane extends Label {
+		private StringBuilder sb = new StringBuilder();
+
+		private void setScaleText(int scaleNumerator, int scaleDenominator) {
+			sb.setLength(0);
+			sb.append("x");
+			sb.append(scaleNumerator);
+			if (scaleDenominator > 1) {
+				sb.append("/");
+				sb.append(scaleDenominator);
+			}
+			this.setText(sb.toString());
+		}
 	}
 
 	private static class MouseClickEventHandler implements EventHandler<MouseEvent> {
@@ -163,21 +178,44 @@ public class ImageViewProcessor {
 	}
 
 	private static class MouseScrollEventHandler implements EventHandler<ScrollEvent> {
-		private Node image;
+		private ImageView image;
+		private ScaleTextPane scaleTextPane;
+		private int scaleNumerator;
+		private int scaleDenominator;
+		private double imageWidth;
+		private double imageHeight;
 
-		public MouseScrollEventHandler(Node image) {
-			this.image = image;
+		public MouseScrollEventHandler(ImageView imageView, ScaleTextPane scaleTextPane, int scaleNumerator,  int scaleDenominator) {
+			this.image = imageView;
+			this.scaleTextPane = scaleTextPane;
+			this.scaleNumerator = scaleNumerator;
+			this.scaleDenominator = scaleDenominator;
+			Image i = image.getImage();
+			this.imageWidth = i.getWidth();
+			this.imageHeight = i.getHeight();
 		}
 
 		@Override
 		public void handle(ScrollEvent event) {
 			double scrollY = event.getDeltaY();
 			if (scrollY < 0d) {
-				image.setScaleX(image.getScaleX() * 0.5d);
-				image.setScaleY(image.getScaleY() * 0.5d);
+				if (scaleNumerator <= 1) {
+					scaleDenominator <<= 1;
+				} else {
+					scaleNumerator >>= 1;
+				}
+				image.setFitWidth(imageWidth * scaleNumerator / scaleDenominator);
+				image.setFitHeight(imageHeight * scaleNumerator / scaleDenominator);
+				scaleTextPane.setScaleText(scaleNumerator, scaleDenominator);
 			} else if (scrollY > 0d) {
-				image.setScaleX(image.getScaleX() * 2d);
-				image.setScaleY(image.getScaleY() * 2d);
+				if (scaleDenominator <= 1) {
+					scaleNumerator <<= 1;
+				} else {
+					scaleDenominator >>= 1;
+				}
+				image.setFitWidth(imageWidth * scaleNumerator / scaleDenominator);
+				image.setFitHeight(imageHeight * scaleNumerator / scaleDenominator);
+				scaleTextPane.setScaleText(scaleNumerator, scaleDenominator);
 			}
 			event.consume();
 		}
